518636d77a0360e24444d59b0e28c5632bfe5f0a
heathcetide <2148582258@qq.com>
2025-12-19 19:23:15 +0800

    feat: 删除客户端本地FS

--- ui/HibiscusFS/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-// middlewares/errorHandler.js
-function errorHandler(err, req, res, next) {
-    console.error('[Error]', err.message);
-    res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
-}
-
-module.exports = errorHandler;
